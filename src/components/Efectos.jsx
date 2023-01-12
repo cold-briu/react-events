cfd5e17142e94e1ec222082c97b55c518e166ba3
Andrés Fernández <andresavena19@gmail.com>
2023-01-12 16:30:48 -0500

    http effects

--- src/components/Efectos.jsx
+++ src/components/Efectos.jsx
@@ -1,12 +1,25 @@
 import { useEffect, useState } from "react";
+import * as api from "../api"
 
 const Efectos = () => {
 	const [count, setCount] = useState(0)
 	const [sorpresa, setSorpresa] = useState(10)
 
+	const [loading, setLoading] = useState(false)
+	const [data, setData] = useState(false)
+
+	const getData = async () => {
+		setLoading(true)
+		let data = await api.getPlanets()
+		console.log(data);
+		setLoading(false)
+		setData(data)
+
+	}
+
 	// ejecutar una vez usando arr vacio como segundo parametro
 	useEffect(() => {
-		console.log("@@ una sola vez");
+		getData()
 	}, [])
 
 
@@ -36,7 +49,14 @@ const Efectos = () => {
 	return <>
 		{console.log("render")}
 		<h3 className="m-2">soy Efectos.jsx</h3>
+
+		{loading && <h3 className="m-2"> :D cargando...</h3>}
+
+		{data && data.results.map(e => <p>{e.name}</p>)}
+
+
 		<div className="p-5 d-flex flex-column">
+
 
 			<button onClick={handleClick}
 				className="btn btn-primary">
